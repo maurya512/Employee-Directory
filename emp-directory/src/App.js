73f4fd57758821eba891952abe1dfd7112a1c5bd
Maurya Patel <patelmaurya0512@gmail.com>
2020-09-20 23:26:12 -0400

    imported and referenced correct files

--- emp-directory/src/App.js
+++ emp-directory/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App";
+import axios from "axios";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    employees: []
+  };
+
+  componentDidMount() {
+    axios.get("https://randomuser.me/api/?results=200&nat=us")
+    .then(res => {
+      this.setState({ employees: res.data.results});
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        {this.state.employees.length > 0 &&
+        <Navbar employees={this.state.employees}></Navbar>}
+      </div>
+    );
+  }
 }
+
 
 export default App;
