e10845ba4fba12e43f3e2a860f14a22b1f348909
Maurya Patel <patelmaurya0512@gmail.com>
2020-09-21 21:57:06 -0400

    Updates

---
+++ precache-manifest.7a92b6e52398215bd93d5d2d162efa27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5c8e82897ce655b498b6e19b3c7184",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "1eff6173b2ff5f4897ab",
+    "url": "/Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "08b1a54f39d7bacd68c5",
+    "url": "/Employee-Directory/static/css/main.448ab345.chunk.css"
+  },
+  {
+    "revision": "1eff6173b2ff5f4897ab",
+    "url": "/Employee-Directory/static/js/2.8b358c2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.8b358c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b1a54f39d7bacd68c5",
+    "url": "/Employee-Directory/static/js/main.eb482af7.chunk.js"
+  },
+  {
+    "revision": "81f525cb638594ce199e",
+    "url": "/Employee-Directory/static/js/runtime-main.edcec86b.js"
+  }
+]);
